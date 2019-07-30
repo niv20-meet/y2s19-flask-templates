c2b232b2e2a27f54cf222e7a633a7c7b9b4ceaef
myname21-meet <myname21@meet.mit.edu>
2019-07-30 12:07:51 +0300

    sdf

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "Hello World"
+	food= ["pasta","pizza","baget"]
+	food=food   
+	return render_template("index.html",food=food)
+
 
 if __name__ == '__main__':
    app.run(debug = True)
